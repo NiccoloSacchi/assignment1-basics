ce84092d18f1765a13618e6b2a545728173a17b2
Niccolo Sacchi <niccsacchi@gmail.com>
2025-10-12 21:09:05 +0200

    Refactor training.

--- cs336_basics/train_llm.py
+++ cs336_basics/train_llm.py
@@ -1,6 +1,6 @@
 """Train an LLM on the given dataset and with the specified configuration.
 
-Example usage for TinyStories dataset:
+Example usages for TinyStories dataset:
   .venv/bin/python cs336_basics/train_llm.py \
     --train_tokens_path /Users/niccolosacchi/assignment1-basics/data/TinyStoriesV2-GPT4-train-tokens.npy \
     --val_tokens_path /Users/niccolosacchi/assignment1-basics/data/TinyStoriesV2-GPT4-valid-tokens.npy \
@@ -22,8 +22,22 @@ Example usage for TinyStories dataset:
     --validation_interval 100 \
     --log_dir /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model \
     --checkpoint_dir /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model \
+    --checkpoint_interval 100
+    
+  .venv/bin/python cs336_basics/train_llm.py \
+    --train_tokens_path /Users/niccolosacchi/assignment1-basics/data/TinyStoriesV2-GPT4-train-tokens.npy \
+    --val_tokens_path /Users/niccolosacchi/assignment1-basics/data/TinyStoriesV2-GPT4-valid-tokens.npy \
+    --device mps \
+    --warmup_iters 1000 \
+    --max_learning_rate 1e-3 \
+    --min_learning_rate 1e-5 \
+    --batch_size 16 \
+    --total_tokens 327680000 \
+    --validation_interval 100 \
+    --log_dir /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model \
+    --checkpoint_dir /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model \
     --checkpoint_interval 100 \
-    --load_checkpoint /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model/checkpoint_6.pt
+    --load_checkpoint /Users/niccolosacchi/assignment1-basics/model/TinyStories/small_model/checkpoint_10.pt
   
 Example usage for OpenWebText dataset:
   .venv/bin/python cs336_basics/train_llm.py \
@@ -37,12 +51,12 @@ Example usage for OpenWebText dataset:
 import argparse
 import os
 import torch
-from cs336_basics.utils import (
+from cs336_basics.io import (
   read_byte_file_to_memmap,
-  get_batch,
   load_checkpoint,
   save_checkpoint,
 )
+from cs336_basics.utils import  get_batch
 from cs336_basics.model import TransformerLM
 from cs336_basics.optimizer import (
   AdamW,
@@ -78,31 +92,31 @@ parser.add_argument(
 
 # Model hyperparameters.
 parser.add_argument(
-  '--vocab_size', type=int, required=True,
+  '--vocab_size', type=int, required=False,
   help='Vocabulary size used to obtain the --train_tokens_path and --val_tokens_path files.',
 )
 parser.add_argument(
-  '--context_length', type=int, required=True,
+  '--context_length', type=int, required=False,
   help='Context length used for training.',
 )
 parser.add_argument(
-  '--num_layers', type=int, required=True,
+  '--num_layers', type=int, required=False,
   help='Number of TransformerBlock layers.',
 )
 parser.add_argument(
-  '--d_model', type=int, required=True,
+  '--d_model', type=int, required=False,
   help='Hidden dimension of the model.',
 )
 parser.add_argument(
-  '--num_heads', type=int, required=True,
+  '--num_heads', type=int, required=False,
   help='Number of attention heads.',
 )
 parser.add_argument(
-  '--d_ff', type=int, required=True,
+  '--d_ff', type=int, required=False,
   help='Dimensionality of the position-wise feed-forward inner layer.',
 )
 parser.add_argument(
-  '--rope_theta', type=float, required=True,
+  '--rope_theta', type=float, required=False,
   help='If not None, use RoPE with the given base value to compute the rotation angles.',
 )
 parser.add_argument(
@@ -112,11 +126,11 @@ parser.add_argument(
 
 # Optimizer hyperparameters
 parser.add_argument(
-  '--betas', type=float, nargs=2, required=True,
+  '--betas', type=float, nargs=2, required=False,
   help='Beta coefficients for AdamW.',
 )  
 parser.add_argument(
-  '--weight_decay', type=float, required=True,
+  '--weight_decay', type=float, required=False,
   help='Weight decay coefficient for AdamW.',
 )
 
@@ -159,7 +173,7 @@ parser.add_argument(
 )
 parser.add_argument(
   '--load_checkpoint', type=str, default=None, required=False,
-  help='Optional. Path to checkpoint file to resume training from.',
+  help='Optional. Path to checkpoint file to resume training from. If passed, the above model and optimizer hyperparameters are ignored.',
 )
 
 # Logging.
@@ -183,38 +197,68 @@ print(f"Train data shape: {train_data.shape}, dtype: {train_data.dtype}")
 print(f"Val data shape: {val_data.shape}, dtype: {val_data.dtype}")
 
 # ============================================================================
+# LOAD CHECKPOINT IF PROVIDED
+# ============================================================================
+start_iteration = 0
+model = None
+optimizer = None
+if args.load_checkpoint:
+  print("============================================================================")
+  print(f"Loading checkpoint from {args.load_checkpoint}")
+  model, optimizer, last_iteration = load_checkpoint(
+    args.load_checkpoint,
+    TransformerLM,
+    AdamW,
+    device=args.device,
+  )
+  start_iteration = last_iteration + 1
+  print(f"Resuming training from iteration {start_iteration}")
+  print("============================================================================")
+  device = next(model.parameters()).device
+  print(f"Model loaded from checkpoint and instantiated on device {device}:")
+  print(model)
+  print("============================================================================")
+  print(f"Optimizer loaded from checkpoint and instantiated:")
+  print(optimizer)
+
+  
+# ============================================================================
 # INSTANTIATE THE MODEL
 # ============================================================================
-model = TransformerLM(
-  vocab_size=args.vocab_size,
-  context_length=args.context_length,
-  num_layers=args.num_layers,
-  d_model=args.d_model,
-  num_heads=args.num_heads,
-  d_ff=args.d_ff,
-  rope_theta=args.rope_theta,
-  device=torch.device(args.device),
-  dtype=torch.float32,
-)
-print("============================================================================")
-print(f"Model instantiated on device {args.device}:")
-print(model)
+if model is None:
+  model = TransformerLM(
+    vocab_size=args.vocab_size,
+    context_length=args.context_length,
+    num_layers=args.num_layers,
+    d_model=args.d_model,
+    num_heads=args.num_heads,
+    d_ff=args.d_ff,
+    rope_theta=args.rope_theta,
+    device=torch.device(args.device),
+    dtype=torch.float32,
+  )
+  print("============================================================================")
+  device = next(model.parameters()).device
+  print(f"Model instantiated on device {device}:")
+  print(model)
 
 # ============================================================================
 # INSTANTIATE THE OPTIMIZER AND LEARNING RATE SCHEDULER
 # ============================================================================
-optimizer = AdamW(
-  model.parameters(),
-  lr=args.min_learning_rate,  # Not really used, as we use a scheduler.
-  betas=args.betas,
-  eps=1e-8,
-  weight_decay=args.weight_decay,
-)
-print("============================================================================")
-print(f"Optimizer instantiated:")
-print(optimizer)
+if optimizer is None:
+  optimizer = AdamW(
+    model.parameters(),
+    lr=args.min_learning_rate,  # Not really used, as we use a scheduler.
+    betas=args.betas,
+    eps=1e-8,
+    weight_decay=args.weight_decay,
+  )
+  print("============================================================================")
+  print(f"Optimizer instantiated:")
+  print(optimizer)
 
-total_iterations = args.total_tokens // (args.batch_size * args.context_length)
+context_length = model.init_args()["context_length"]
+total_iterations = args.total_tokens // (args.batch_size * context_length)
 scheduler = CosineLearningRateScheduler(
   optimizer,
   max_learning_rate=args.max_learning_rate,
@@ -222,18 +266,6 @@ scheduler = CosineLearningRateScheduler(
   warmup_iters=args.warmup_iters,
   cosine_cycle_iters=total_iterations,
 )
-
-# ============================================================================
-# LOAD CHECKPOINT IF PROVIDED
-# ============================================================================
-start_iteration = 0
-if args.load_checkpoint:
-  print("============================================================================")
-  print(f"Loading checkpoint from {args.load_checkpoint}")
-  last_iteration = load_checkpoint(args.load_checkpoint, model, optimizer)
-  start_iteration = last_iteration + 1
-  print(f"Resuming training from iteration {start_iteration}")
-  print("============================================================================")
 
 # ============================================================================
 # SETUP LOGGING AND CHECKPOINT DIRECTORIES
@@ -245,54 +277,6 @@ if args.log_dir:
 
 if args.checkpoint_dir:
   os.makedirs(args.checkpoint_dir, exist_ok=True)
-  
-  # Save CLI command for loading checkpoints in a readme file.
-  readme_path = os.path.join(args.checkpoint_dir, "README.md")
-  cli_command = f"""# Model Checkpoints Directory
-Resume training from a checkpoint in this directory with the following command:
-```bash
-.venv/bin/python cs336_basics/train_llm.py \\
---train_tokens_path {args.train_tokens_path} \\
---val_tokens_path {args.val_tokens_path} \\
---vocab_size {args.vocab_size} \\
---context_length {args.context_length} \\
---num_layers {args.num_layers} \\
---d_model {args.d_model} \\
---num_heads {args.num_heads} \\
---d_ff {args.d_ff} \\
---rope_theta {args.rope_theta} \\
---device {args.device} \\
---betas {args.betas[0]} {args.betas[1]} \\
---weight_decay {args.weight_decay} \\
---warmup_iters {args.warmup_iters} \\
---max_learning_rate {args.max_learning_rate} \\
---min_learning_rate {args.min_learning_rate} \\
---batch_size {args.batch_size} \\
---total_tokens {args.total_tokens} \\
---validation_interval {args.validation_interval} \\
---log_dir {args.log_dir} \\
---checkpoint_dir {args.checkpoint_dir} \\
---checkpoint_interval {args.checkpoint_interval} \\
---load_checkpoint <checkpoint_path>"""
-
-  # Add optional arguments if they exist
-  if args.train_metadata_path:
-    cli_command += f" \\\n  --train_metadata_path {args.train_metadata_path}"
-  if args.val_metadata_path:
-    cli_command += f" \\\n  --val_metadata_path {args.val_metadata_path}"
-  
-  cli_command += "\n```\n"
-  
-  create_readme = True
-  if os.path.exists(readme_path):
-    print("============================================================================")
-    create_readme = input(f"{readme_path} already exists. Overwrite? (y/n): ")
-    if create_readme.lower() != 'y':
-      print("Skipping README.md creation.")
-      create_readme = False
-  if create_readme:
-    with open(readme_path, "w") as f:
-      f.write(cli_command)
 
 # ============================================================================
 # TRAINING LOOP
@@ -306,7 +290,7 @@ for iteration in range(start_iteration, total_iterations):
   x, y = get_batch(
     dataset=train_data,
     batch_size=args.batch_size,
-    context_length=args.context_length,
+    context_length=context_length,
     device=args.device,
     dtype=torch.int32,
   )
@@ -330,7 +314,7 @@ for iteration in range(start_iteration, total_iterations):
       val_x, val_y = get_batch(
         dataset=val_data,
         batch_size=args.batch_size,
-        context_length=args.context_length,
+        context_length=context_length,
         device=args.device,
         dtype=torch.int32,
       )
@@ -339,8 +323,9 @@ for iteration in range(start_iteration, total_iterations):
     loss_message += f" | val_loss={val_loss.item():.4f}"
   else:
     loss_message += " | val_loss=------"
-  tokens_processed = iteration * args.batch_size * args.context_length
-  print(f"{loss_message} | lr={lr:.6f} | tokens processed={tokens_processed:>{tokens_width}}/{args.total_tokens}")
+  tokens_processed = iteration * args.batch_size * context_length
+  loss_message = f"{loss_message} | lr={lr:.6f} | tokens processed={tokens_processed:>{tokens_width}}/{args.total_tokens}"
+  print(loss_message)
 
   # Log training loss.
   if args.log_dir:
